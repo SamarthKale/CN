56f84c8e01b458d903507948266a167270983022
Samarth Kale <70852071+SamarthKale@users.noreply.github.com>
2026-05-06 22:34:25 +0530

    Refactor client.py for continuous messaging

--- client.py
+++ client.py
@@ -1,15 +1,9 @@
 import socket
 
 s = socket.socket()
+s.connect(('localhost', 5000))
 
-s.connect(("localhost", 12345))
-
-# send custom message
-msg = input("Enter message for server: ")
-s.send(msg.encode())
-
-# receive from server
-reply = s.recv(1024).decode()
-print("Server:", reply)
-
-s.close()
+while True:
+    msg = input("You: ")
+    s.send(msg.encode())
+    print("Server:", s.recv(1024).decode())
