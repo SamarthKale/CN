c91091af776b64a25024955fdecde251b99201ca
Samarth Kale <70852071+SamarthKale@users.noreply.github.com>
2026-05-06 22:33:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,12 @@
 import socket
 
 s = socket.socket()
-
-s.bind(("localhost", 12345))
-
+s.bind(('localhost', 5000,))
 s.listen(1)
-
-print("Waiting for client...")
 
 c, addr = s.accept()
 
-print("Connected with", addr)
-
-# receive from client
-msg = c.recv(1024).decode()
-print("Client:", msg)
-
-# send custom message
-reply = input("Enter message for client: ")
-c.send(reply.encode())
-
-c.close()
-s.close()
+while True:
+    print("Client:", c.recv(1024).decode())
+    msg = input("You: ")
+    c.send(msg.encode())
